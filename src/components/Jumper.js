235fc94eb0e14db6ac942e9690d894fbf2f78739
erikhaandrikman <e.haandrikman@metrological.com>
2020-02-06 14:54:13 +0100

    Updated Jumper

--- src/components/Jumper.js
+++ src/components/Jumper.js
@@ -5,12 +5,25 @@ export default class Jumper extends Lightning.Component {
 
     static _template() {
         return {
-            rect: true
+            Bottom:{y:21,
+                src: Utils.asset("assets/level/jumper-a.png")
+            },
+            Top:{ y:10, x:5,
+                src: Utils.asset("assets/level/jumper-b.png")
+            }
         };
     }
 
     _construct() {
         this._size = new Vector(0.4, 0.4);
+    }
+
+    _init(){
+        this._animation = this.tag("Top").animation({
+            duration:0.5, actions:[
+                {p:'y', v:{0:10, 0.1:-5, 1:10}}
+            ]
+        });
     }
 
 
@@ -27,9 +40,12 @@ export default class Jumper extends Lightning.Component {
         return "jumper";
     };
 
-    act(dt) {}
+    act(dt) {
+
+    }
 
     onTouch(player) {
+        this._animation.start();
         player.speed.y = -25;
     }
 }
